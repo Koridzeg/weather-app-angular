65b0eec9f736f40f7724fca30fc6294edca77102
KoridzeG <giorgiqori@yahoo.com>
2023-02-22 21:15:40 +0400

    feat: refactoring & displaying weatherinfo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
-  imports: [BrowserModule, AppRoutingModule, GoogleMapsModule,HttpClientModule],
+  declarations: [AppComponent, MapComponent,],
+  imports: [BrowserModule, AppRoutingModule, GoogleMapsModule,HttpClientModule,MatDialogModule,MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,23 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 
 declare var google: any;
+
+interface MapClickEvent extends google.maps.MapMouseEvent {
+  latLng: google.maps.LatLng;
+}
+
+interface OpenMeteoResponse {
+  current_weather: {
+    time: string;
+    temperature: number;
+    weathercode: number;
+    windspeed: number;
+    winddirection: number;
+  };
+  hourly: {
+    time: string[];
+    windspeed_10m: number[];
+    temperature_2m: number[];
+    relativehumidity_2m: number[];
+  };
+}
+
+interface Marker {
+  lat: number;
+  lng: number;
+  draggable: boolean;
+}
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
+  providers: [
+    {
+      provide: MatDialogRef,
+      useValue: {},
+    },
+    {
+      provide: MAT_DIALOG_DATA,
+      useValue: {},
+    },
+  ],
 })
 export class MapComponent implements OnInit {
   lat = 51.678418;
   lng = 7.809007;
   zoom = 12;
-  map: any;
-  marker: any;
-  hours!: any[];
+  map!: google.maps.Map;
+  marker!: google.maps.Marker;
+  hours: any[] = [];
+  selectedMarker!: Marker;
+  showHourly = true;
 
-  constructor(private http: HttpClient) {}
+  showHourlyweather() {
+    this.showHourly = true;
+  }
+
+  constructor(private http: HttpClient, public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.mapInitializer();
@@ -26,38 +73,37 @@ export class MapComponent implements OnInit {
   mapInitializer() {
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: this.lat, lng: this.lng },
-      zoom: this.zoom
+      zoom: this.zoom,
     });
-  
-    this.map.addListener('click', (event: any) => {
+
+    const infowindow = new google.maps.InfoWindow();
+
+    this.map.addListener('click', (event: MapClickEvent) => {
       if (this.marker) {
         this.marker.setMap(null);
       }
       this.marker = new google.maps.Marker({
         position: event.latLng,
-        map: this.map
+        map: this.map,
       });
-  
+
       this.lat = event.latLng.lat();
       this.lng = event.latLng.lng();
-  
+
       const url = `https://api.open-meteo.com/v1/forecast?latitude=${this.lat}&longitude=${this.lng}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`;
-  
-      this.http.get(url).pipe(
-        map((response: any) => {
-          console.log(response)
-          const currentWeather = response.current_weather;
-          const hourlyForecast = response.hourly;
-          return {
-            currentTemperature: currentWeather.temperature,
-            currentHumidity: currentWeather.relativehumidity_2m,
-            currentWindSpeed: currentWeather.windspeed_10m,
-            hourlyForecast: hourlyForecast
-          };
-        })
-      ).subscribe((data: any) => {
-        console.log(data);
-        // Do something with the data
+
+      this.http.get<OpenMeteoResponse>(url).subscribe((data) => {
+        infowindow.setContent(
+          `<mat-card>
+          <mat-card-title> Weather Information </mat-card-title>
+          <mat-card-content>
+          <p> Temperature : ${data.current_weather.temperature}</p> 
+          <p> Wind Speed : ${data.current_weather.windspeed} </p>
+          <button mat-raised-button color="primary" (click)="showHourlyWeather()">Show Hourly Weather </button>
+          </mat-card-content>         
+          </mat-card>`
+        );
+        infowindow.open(this.map, this.marker);
       });
     });
   }
